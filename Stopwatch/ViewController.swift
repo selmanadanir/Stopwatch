e019da998bb5bd5bbcf9cddad1d1881a3780f1fd
selman adanir <adanirselman@gmail.com>
2022-07-17 14:50:00 +0300

    finish code

--- Stopwatch/ViewController.swift
+++ Stopwatch/ViewController.swift
@@ -9,7 +9,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
-
+    var time = 0
+    var timer = Timer()
+    
     @IBOutlet weak var timerLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -17,18 +19,26 @@ class ViewController: UIViewController {
     }
 
     @IBAction func startButton(_ sender: Any) {
-
+        
+        timerLabel.text = String(time)
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(stopWatchTimerSelector), userInfo: nil, repeats: true)
     }
     
     @IBAction func stopButton(_ sender: Any) {
-
+        
+        timer.invalidate()
     }
 
     @IBAction func resetButton(_ sender: Any) {
-
+        timer.invalidate()
+        time = 0
+        timerLabel.text = String(time)
+        
     }
     
     @objc func stopWatchTimerSelector(){
-
+        
+        timerLabel.text = String(time)
+        time += 1
     }
 }
